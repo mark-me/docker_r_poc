b0e261b5d4a318e93e46498b15bd5bcb5cb292b8
Mark Zwart <mark.zwart@pobox.com>
2019-06-16 21:24:04 +0000

    Added pandoc

--- r_container/make_report.R
+++ r_container/make_report.R
@@ -1,15 +1,15 @@
 #* @get /make_report
 make_report <- function(id_company){
-  # convert the input to a number
 
-  read_yaml("config.yml")
+  library(yaml)
+  config <- read_yaml("config.yml")
   config$id_company <- id_company
   write_yaml(config, "config.yml") 
   
   # Render report
-  #rmarkdown::render("report.Rmd",
-  #                  output_file="~/report/report/report.html",
-  #                  encoding = "utf-8")
+  rmarkdown::render("report.Rmd",
+                    output_file="~/report/report/report.html",
+                    encoding = "utf-8")
 
   list(msg = paste0("The message is: '", id_company, "'"))
 }
