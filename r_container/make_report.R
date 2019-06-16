3873ab35d63492c3d5ded61ff75311b2bb40cb3b
Mark Zwart <mark-me@users.noreply.github.com>
2019-06-16 22:32:57 +0200

    Update make_report.R

--- r_container/make_report.R
+++ r_container/make_report.R
@@ -6,10 +6,10 @@ make_report <- function(id_company){
   config$id_company <- id_company
   write_yaml(config, "config.yml") 
   
-  list(msg = paste0("The message is: '", id_company, "'"))
   # Render report
-  #rmarkdown::render("report.Rmd",
-  #                  output_file="~/report/report/report.html",
-  #                  encoding = "utf-8")
-  
+  rmarkdown::render("report.Rmd",
+                    output_file="~/report/report/report.html",
+                    encoding = "utf-8")
+
+  list(msg = paste0("The message is: '", id_company, "'"))
 }
